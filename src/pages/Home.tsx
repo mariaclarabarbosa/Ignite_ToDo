a31347ac097cda0451cbbd63df9a3d666503134d
mariaclarabarbosa <clara_estrella@protonmail.com>
2022-02-06 21:35:14 -0300

    Implementação da lógica de adicionar tarefa

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,11 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    setTasks([...tasks, {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }]);
   }
 
   function handleToggleTaskDone(id: number) {
